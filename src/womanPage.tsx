233a0d1c9797b9d47be6cad65b4442edfb2a61b4
Purpkin <guiejo2020@gmail.com>
2025-04-05 23:24:04 -0300

    Align paragraphs by middle

--- src/womanPage.tsx
+++ src/womanPage.tsx
@@ -16,16 +16,16 @@ const WomanPage = ({ woman }: Props) => {
     return (
         <div className="w-8/12 min-h-[90vh] flex flex-col mx-auto items-center">
 
-            <div className="w-1/2 h-20">
+            <div className="w-1/2 h-20" tabIndex={0}>
                 <div className="w-96 border-t-2 mb-4 mx-auto border-contrast animate-reveal-div" />
                 <h1 className='font-bold text-4xl text-center text-contrast animate-reveal-div'>{woman.name}</h1>
             </div>
 
             {
                 woman.paragraphs.map((paragraph, index) => (
-                    <div className={"w-full mb-8 flex " + `${index % 2 === 0 ? "animate-paragraph-even flex-row" : "animate-paragraph flex-row-reverse"}`} key={index}>
+                    <div className={"w-full mb-8 flex " + `${index % 2 === 0 ? "animate-paragraph-even flex-row" : "animate-paragraph flex-row-reverse"}`} key={index} tabIndex={0}>
                         <HoverImage className='' imageUrl={paragraph.imagePath} />
-                        <div className="grow h-64 py-4 px-6 text-base 2xl:text-lg">
+                        <div className="growA h-64 py-4 px-6 flex flex-row items-center text-base 2xl:text-lg">
                             <p className='indent-10 text-justify text-contrast'>
                                 {paragraph.text}
                             </p>
